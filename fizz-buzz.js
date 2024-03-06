9359ecb9f484a9aa53f06f91a7c84501787d38e6
minhaj.dev <minhajhasan.dev@gmail.com>
2024-03-06 10:41:23 +0600

    used for loop for generation numbers and conditionals to check the required condition to match

--- fizz-buzz.js
+++ fizz-buzz.js
@@ -1,2 +1,11 @@
-const randomNumber = Math.floor(Math.random() * 100);
-
+for (let i = 1; i <= 100; i++) {
+  if (i % 3 === 0 && i % 5 === 0) {
+    console.log("FizzBuzz");
+  } else if (i % 3 === 0) {
+    console.log("Fizz");
+  } else if (i % 5 === 0) {
+    console.log("Buzz");
+  } else {
+    console.log(i);
+  }
+}
